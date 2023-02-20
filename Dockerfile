42a62ae06b1818bc48d6ea2439c00bf80782c7ef
Dave Fennessey <dave@dfennessey.com>
2023-02-20 12:04:24 +0100

    Making scrapyrt play nicely with docker Removing test files

--- Dockerfile
+++ Dockerfile
@@ -7,4 +7,4 @@ RUN pip3 install --no-cache-dir --upgrade -r /code/requirements.txt
 
 COPY . /code
 
-CMD scrapyrt -p $PORT
+CMD exec scrapyrt -i 0.0.0.0 -p $PORT
--- start-quotes-spider.py
+++
@@ -1,7 +0,0 @@
-from scrapy.crawler import CrawlerProcess
-from scrapy.utils.project import get_project_settings
-from goodreads_scraper.spiders import TestSpider
-
-process = CrawlerProcess(get_project_settings())
-process.crawl(TestSpider)
-process.start()
