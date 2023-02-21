bb96a2a7a02d66a8959daa421f985b5d2e6e8fa7
Dave Fennessey <dave@dfennessey.com>
2023-02-21 17:32:22 +0100

    Adding the big baddie book scraper Adding its items isbnlib is awesome

--- goodreads_scraper/items.py
+++ goodreads_scraper/items.py
@@ -2,13 +2,15 @@
 #
 # See documentation in:
 # https://docs.scrapy.org/en/latest/topics/items.html
+import json
 import re
 from datetime import datetime
 
+import isbnlib
 import scrapy
 from dateutil.parser import parse
 from itemloaders import ItemLoader
-from itemloaders.processors import MapCompose, TakeFirst
+from itemloaders.processors import MapCompose, TakeFirst, Compose
 from scrapy import Field
 
 
@@ -19,6 +21,67 @@ def safe_parse_date(date):
     except ValueError:
         date = None
     return date
+
+
+def convert_epoch_to_timestamp(epoch):
+    epoch_seconds = epoch / 1000
+    time_object = datetime.fromtimestamp(epoch_seconds)
+    return time_object.date().isoformat()
+
+
+def convert_ordered_list_to_ratings_dict(rating_list):
+    rating_dict = {idx + 1: rating for idx, rating in enumerate(rating_list)}
+    return json.dumps(rating_dict)
+
+
+def filter_asin(asin):
+    if asin and len(str(asin)) == 10:
+        return asin
+    return None
+
+
+def isbn_filter(isbn):
+    if isbn and isbnlib.is_isbn10(isbn):
+        return isbn
+
+
+def isbn13_filter(isbn):
+    if isbn and isbnlib.is_isbn13(isbn):
+        return isbn
+
+
+class BookLoader(ItemLoader):
+    default_output_processor = TakeFirst()
+
+
+class BookItem(scrapy.Item):
+    # High Level Info
+    book_url = Field()
+    title = Field(input_processor=MapCompose(str.strip))
+    author = Field(input_processor=MapCompose(str.strip))
+    author_url = Field(input_processor=MapCompose(str.strip))
+    book_description = Field()
+
+    # Work Details
+    work_internal_id = Field()
+    work_id = Field(serializer=int)
+    publish_date = Field(input_processor=MapCompose(convert_epoch_to_timestamp))
+    original_title = Field(input_processor=MapCompose(str.strip))
+
+    # Work Statistics
+    num_ratings = Field()
+    num_reviews = Field()
+    avg_rating = Field()
+    rating_histogram = Field(input_processor=convert_ordered_list_to_ratings_dict)
+
+    # Book Statistics
+    num_pages = Field()
+    language = Field(input_processor=MapCompose(str.strip))
+    isbn = Field(input_processor=MapCompose(str.strip, isbn_filter))
+    isbn13 = Field(input_processor=MapCompose(str.strip, isbn13_filter))
+    asin = Field(input_processor=MapCompose(filter_asin))
+    series = Field(input_processor=MapCompose(str.strip))
+    genres = Field(output_processor=Compose(set, list))
 
 
 class UserReviewItem(scrapy.Item):
---
+++ goodreads_scraper/spiders/book_spider.py
@@ -0,0 +1,182 @@
+"""Spider to extract information from a /book/show type page on Goodreads"""
+import json
+import re
+from typing import Dict, List
+from urllib.parse import urlsplit
+
+import isbnlib
+import scrapy
+from scrapy import Request
+
+from ..items import BookLoader, BookItem
+
+TYPENAME = "__typename"
+
+
+class BookSpider(scrapy.Spider):
+    """Extract information from a /book/show type page on Goodreads"""
+    name = "book"
+
+    def __init__(self, books="/book/show/41745268-gone-hunting"):
+        super().__init__()
+        self.start_urls = books.split(",")
+
+    def start_requests(self):
+        for url in self.start_urls:
+            converted_url = self._format_book_url(url)
+            yield Request(converted_url, callback=self.parse, dont_filter=True)
+
+    def parse(self, response):
+        self.logger.info("New Book Response")
+        return self.parse_book(response)
+
+    def parse_book(self, response, loader=None):
+        if not loader:
+            loader = BookLoader(BookItem(), response=response)
+
+        text_body = response.xpath('//*[@id="__NEXT_DATA__"]/text()').get()
+        parsed_json_body = json.loads(text_body)
+        book_info = parsed_json_body['props']['pageProps']['apolloState']
+
+        contributor = self._take_largest_element(book_info, "Contributor")
+        series = self._take_first_element(book_info, "Series")
+        work = self._take_largest_element(book_info, "Work")
+        book = self._take_largest_element(book_info, "Book")
+        book_details = book.get("details")
+
+        backup_isbns = self._extract_isbn_from_affiliates(
+            book.get("links({})", {}).get("secondaryAffiliateLinks", list()))
+
+        book_url = urlsplit(response.request.url).path
+
+        if not book:
+            self.logger.warning("Unable to load body, reloading page!")
+            converted_url = self._format_book_url(book_url)
+            return Request(converted_url, callback=self.parse, dont_filter=True)
+
+        # High Level Info
+        loader.add_value('book_url', book_url)
+        loader.add_value('title', book.get("title"))
+        loader.add_value('author', contributor.get("name"))
+        loader.add_value('author_url', contributor.get("webUrl"))
+        loader.add_value('book_description', book.get('description({"stripped":true})'))
+
+        # Work Details
+        loader.add_value('work_internal_id', work.get("id"))
+        loader.add_value('work_id', work.get("legacyId"))
+        loader.add_value('publish_date', work.get("details").get("publicationTime"))
+        loader.add_value('original_title', work.get("details").get("originalTitle"))
+
+        # Work Statistics
+        loader.add_value('num_ratings', work.get("stats").get("ratingsCount"))
+        loader.add_value('num_reviews', work.get("stats").get("textReviewsCount"))
+        loader.add_value('avg_rating', work.get("stats").get("averageRating"))
+        loader.add_value('rating_histogram', work.get("stats").get("ratingsCountDist"))
+
+        # Book Statistics
+        loader.add_value('num_pages', book_details.get("numPages"))
+        loader.add_value('language', book_details.get("language").get("name"))
+        loader.add_value('asin', book_details.get("asin"))
+        loader.add_value('series', series.get("title") if series else "")
+        loader.add_value('genres', self._parse_genres(book.get("bookGenres")))
+
+        # ISBN requires a bit of wrangling
+        isbn = book_details.get("isbn") if book_details.get("isbn") else backup_isbns.get("isbn")
+        isbn13 = book_details.get("isbn13") if book_details.get("isbn13") else backup_isbns.get("isbn13")
+        loader.add_value('isbn', isbn)
+        loader.add_value('isbn13', isbn13)
+
+        return loader.load_item()
+
+    def _take_largest_element(self, input_dict, element_type):
+        largest = None
+        for block in input_dict.values():
+            if block.get(TYPENAME, "") == element_type:
+                if largest is None:
+                    largest = block
+                else:
+                    key_count = self._count_keys_recursive(block)
+                    largest_count = self._count_keys_recursive(largest)
+                    if key_count > largest_count:
+                        largest = block
+            else:
+                continue
+        return largest
+
+    def _parse_genres(self, genre_input_list):
+        parsed_genres = []
+        for genre in genre_input_list:
+            if genre.get(TYPENAME) == "BookGenre":
+                genre_dict = genre.get("genre")
+                if genre_dict.get(TYPENAME) == "Genre":
+                    parsed_genres.append(genre_dict.get("name"))
+        return parsed_genres
+
+    def _take_first_element(self, input_dict, element_type):
+        for block in input_dict.values():
+            if block.get(TYPENAME, "") == element_type:
+                return block
+
+    def _count_keys_recursive(self, input_dict, counter=0):
+        for each_key in input_dict:
+            if isinstance(input_dict[each_key], dict):
+                # Recursive call
+                counter = self._count_keys_recursive(input_dict[each_key], counter + 1)
+            else:
+                counter += 1
+        return counter
+
+    @staticmethod
+    def _format_book_url(user_id_and_name):
+        return f"https://www.goodreads.com{user_id_and_name}"
+
+    @staticmethod
+    def _extract_isbn_from_affiliates(affiliates: List[Dict]) -> Dict[str, str]:
+        """
+        Amazon hates to leave money on the table, so if the book doesn't specifically have an ISBN such as a kindle
+        edition of a book, they'll always try and tie it to a print version of the book with the affiliate links. We
+        can use these affiliates to mine the ISBN and return it with the object
+
+        :param affiliates: List of affiliate dictionaries containing their referral link (which contains ISBN)
+        :return: A dictionary which returns ISBN and ISBN13 if found
+        """
+        isbn_dict = {
+            "isbn": None,
+            "isbn13": None
+        }
+
+        for affiliate in affiliates:
+            match affiliate.get("name"):
+                case "Barnes & Noble":
+                    regex = r"https?:\/\/www\.barnesandnoble\.com\/w\/\?ean=(?P<isbn>[0-9A-Z]{10,13})$"
+                case "Apple Books":
+                    regex = r"https?:\/\/geo\.itunes\.apple\.com\/us\/book\/isbn(?P<isbn>[0-9A-Z]{10,13})\?.*"
+                case "Google Play":
+                    regex = r"https?:\/\/play\.google\.com\/store\/search\?q=(?P<isbn>[0-9A-Z]{10,13})&.*"
+                case "Book Depository":
+                    regex = r"https?:\/\/www\.bookdepository\.com\/search\?searchTerm=(?P<isbn>[0-9A-Z]{10,13})&.*"
+                case "Indigo":
+                    regex = r"https?:\/\/www\.chapters\.indigo\.ca\/en-ca\/home\/search\/\?keywords=(?P<isbn>[0-9A-Z]{10,13}).*"
+                case _:
+                    continue
+
+            results = re.search(regex, affiliate["url"])
+            # The regex messed up, or we weren't able to find an ISBN - just skip it
+            if not results:
+                continue
+
+            regex_results = results.groupdict()
+
+            if "isbn" in regex_results:
+                extracted_isbn = regex_results["isbn"]
+
+                if isbn_dict["isbn"] is None and len(extracted_isbn) == 10 and isbnlib.is_isbn10(extracted_isbn):
+                    isbn_dict["isbn"] = extracted_isbn
+                elif isbn_dict["isbn13"] is None and len(extracted_isbn) == 13 and isbnlib.is_isbn13(extracted_isbn):
+                    isbn_dict["isbn13"] = extracted_isbn
+
+            if isbn_dict["isbn13"] and isbn_dict["isbn"]:
+                # We've filled both entries, no need to go further
+                break
+
+        return isbn_dict
